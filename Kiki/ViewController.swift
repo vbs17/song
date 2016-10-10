ac4e979dfda5da0e7d0a6835840db9a1befafc0f
kei ikeuchi <sister@sister.local>
2016-10-10 20:57:33 +0900

    do

--- Kiki/ViewController.swift
+++ Kiki/ViewController.swift
@@ -1,17 +1,61 @@
 
-
 import UIKit
+import AVFoundation
+
+//録音できたものを次の画面に移す　録音中にマイクの音を拾って波をつける
 
 class ViewController: UIViewController {
-
+    
+    let fileManager = NSFileManager()//録音もできないしそれを再生もできない
+    var audioRecorder: AVAudioRecorder?
+    
+    
+    let fileName = "sample.caf"
+    
+    @IBOutlet weak var viewImage: UIView! //オレンジ色
+    @IBOutlet weak var recordImage: UIButton! //丸いレコーディングボタン
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+         self.setupAudioRecorder()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    
+    //レコード開始
+    @IBAction func recordStart(sender: AnyObject) {
+        audioRecorder?.record()
     }
+    
+    func setupAudioRecorder() {
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord) //マイクから取りこんだ音声データを、そういう形にする
+        try! session.setActive(true)
+        let recordSetting : [String : AnyObject] = [
+            //オーディオデータを設定の通りに全て取りこんでると大量のデータになってしまうので、圧縮
+            AVEncoderAudioQualityKey : AVAudioQuality.Min.rawValue,
+            AVEncoderBitRateKey : 16,//1枚のページにたして16の情報をかけてる
+            AVNumberOfChannelsKey: 2 , //イヤホンも左右から違う音が聞こえる
+            AVSampleRateKey: 44100.0 //これが多ければ多いほどスムーズ
+        ]
+        do {
+            try audioRecorder = AVAudioRecorder(URL: self.documentFilePath(), settings: recordSetting)
+        } catch {
+            print("初期設定でerror")
+        }
+    }
+    
+    // 録音するファイルのパスを取得(録音時、再生時に参照)
+    // swift2からstringByAppendingPathComponentが使えなくなったので少し面倒
+    func documentFilePath()-> NSURL {//要求されたドメインで指定された一般的なディレクトリの Url の配列を返します
+        let urls = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask) as [NSURL]
+        let dirURL = urls[0]
+        return dirURL.URLByAppendingPathComponent(fileName)
+    }                  //作られた新しい URL を返します。
+    
 
-
+    
+    
+    
+    
+    
+    
 }
-
