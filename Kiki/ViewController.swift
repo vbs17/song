04dc96477e9a11c9014e4ba9017a69f450ec0b39
kei ikeuchi <sister@sister.local>
2016-10-11 16:29:50 +0900

    do

--- Kiki/ViewController.swift
+++ Kiki/ViewController.swift
@@ -8,9 +8,16 @@ class ViewController: UIViewController {
     
     let fileManager = NSFileManager()//録音もできないしそれを再生もできない
     var audioRecorder: AVAudioRecorder?
-    
-    
     let fileName = "sample.caf"
+    var time: NSTimer!
+    let recordDelay = 3.0
+    
+    
+    let image1 = UIImage(named: "IMG_2522")
+    let image2 = UIImage(named: "IMG_2844")
+    let image3 = UIImage(named: "IMG_2846")
+    
+    
     
     @IBOutlet weak var viewImage: UIView! //オレンジ色
     @IBOutlet weak var recordImage: UIButton! //丸いレコーディングボタン
@@ -20,14 +27,25 @@ class ViewController: UIViewController {
          self.setupAudioRecorder()
     }
     
-    //レコード開始
-    @IBAction func recordStart(sender: AnyObject) {
-        audioRecorder?.record()
+    //カウントダウンしてレコード開始しボタンのUIも変更
+    @IBAction func recordStart(sender: UIButton) {
+        sender.setImage(UIImage(named: "Kiki5"), forState: UIControlState.Normal)
+    
     }
     
+    func imageiti() {
+        let view1: UIImageView = UIImageView(frame: CGRectMake(0,0,300,300))
+        image1.size = view1
+        
+        
+        
+        
+    }
+    
+    
     func setupAudioRecorder() {
-        let session = AVAudioSession.sharedInstance()
-        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord) //マイクから取りこんだ音声データを、そういう形にする
+        let session = AVAudioSession.sharedInstance() //ここではどっちも設定してる
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord) //マイクから取りこんだ音声データを、再生専用とか録音専用の指定もある
         try! session.setActive(true)
         let recordSetting : [String : AnyObject] = [
             //オーディオデータを設定の通りに全て取りこんでると大量のデータになってしまうので、圧縮
@@ -36,7 +54,7 @@ class ViewController: UIViewController {
             AVNumberOfChannelsKey: 2 , //イヤホンも左右から違う音が聞こえる
             AVSampleRateKey: 44100.0 //これが多ければ多いほどスムーズ
         ]
-        do {
+        do {     //録音したものは/aaa/bbb/ccc.app/Document/sample.caここに入る
             try audioRecorder = AVAudioRecorder(URL: self.documentFilePath(), settings: recordSetting)
         } catch {
             print("初期設定でerror")
@@ -47,9 +65,10 @@ class ViewController: UIViewController {
     // swift2からstringByAppendingPathComponentが使えなくなったので少し面倒
     func documentFilePath()-> NSURL {//要求されたドメインで指定された一般的なディレクトリの Url の配列を返します
         let urls = fileManager.URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask) as [NSURL]
-        let dirURL = urls[0]
+        //iTunesにもバックアップされる、大事なファイルを置くディレクトリへのパスを取得
+        let dirURL = urls[0] //要求されたドメインで指定された一般的なディレクトリの Url の配列のうちの一個が帰ってくる
         return dirURL.URLByAppendingPathComponent(fileName)
-    }                  //作られた新しい URL を返します。
+    }          //その指定したディレクトリにurlを置いて完
     
 
     
